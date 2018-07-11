a667b84b654d551e10ce6472d7fdca26117f4352
Saran Dumronggittigule <saranpol@Sarans-iMac.local>
2018-07-12 05:01:49 +0700

    upload direct to gcs

--- ipa-hub/db_ipa.py
+++ ipa-hub/db_ipa.py
@@ -4,5 +4,6 @@ class DB_IPA(db.Model):
     bundle_id = db.StringProperty()
     version = db.StringProperty()
     name = db.StringProperty()
+    blob_info_key = db.StringProperty()
     created_time = db.DateTimeProperty(auto_now_add=True)
     modified_time = db.DateTimeProperty(auto_now=True)
--- ipa-hub/main.py
+++ ipa-hub/main.py
@@ -4,6 +4,8 @@ import os
 import urllib
 import jinja2
 from db_ipa import *
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -20,9 +22,12 @@ my_default_retry_params = gcs.RetryParams(initial_delay=0.2,
                                           max_retry_period=15)
 gcs.set_default_retry_params(my_default_retry_params)
 
+
+def get_bucket_name() :
+    return os.environ.get('ipa-hub.appspot.com', app_identity.get_default_gcs_bucket_name())
     
 def get_filename(key):
-    bucket_name = os.environ.get('ipa-hub.appspot.com', app_identity.get_default_gcs_bucket_name())
+    bucket_name = get_bucket_name()
     bucket = '/' + bucket_name
     filename = bucket + '/ipa/' + key + '.ipa'
     return filename
@@ -30,18 +35,21 @@ def get_filename(key):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-         template_values = {'test': 'test'}
-         template = JINJA_ENVIRONMENT.get_template('index.html')
-         self.response.write(template.render(template_values))
+        upload_url = blobstore.create_upload_url('/upload', gs_bucket_name = get_bucket_name())
+        template_values = {'upload_url': upload_url}
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
 
-class Upload(webapp2.RequestHandler):
+# class Upload(webapp2.RequestHandler):
+class Upload(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
         bundle_id = self.request.get('bundle_id')
         version = self.request.get('version')
         name = self.request.get('name')
-        ipa = self.request.get('ipa')
+        # ipa = self.request.get('ipa')
         
-        if not bundle_id or not version or not name or not ipa :
+        # if not bundle_id or not version or not name or not ipa :
+        if not bundle_id or not version or not name :
             self.response.write('please check input')
             return
         
@@ -50,25 +58,34 @@ class Upload(webapp2.RequestHandler):
         e.bundle_id = bundle_id
         e.version = version
         e.name = name
+        upload_files = self.get_uploads('ipa')  # 'file' is file upload field in the form
+        blob_info = upload_files[0]
+        e.blob_info_key = str(blob_info.key())
         e.put()
 
-        filename = get_filename(k)
+        # filename = get_filename(k)
 
-        try:
-            write_retry_params = gcs.RetryParams(backoff_factor=1.1)
-            gcs_file = gcs.open(filename,
-                                'w',
-                                content_type='application/octet-stream',
-                                #content_type='image/png',
-                                retry_params=write_retry_params)
-            gcs_file.write(ipa)
-            gcs_file.close()
+        # try:
+        #     write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+        #     gcs_file = gcs.open(filename,
+        #                         'w',
+        #                         content_type='application/octet-stream',
+        #                         #content_type='image/png',
+        #                         retry_params=write_retry_params)
+        #     gcs_file.write(ipa)
+        #     gcs_file.close()
 
-        except Exception, e:  # pylint: disable=broad-except
-            self.response.write('There was an error running!')
-        else:
-            self.response.write('http://ipa-hub.appspot.com/ipa?key='+k)
+        # except Exception, e:  # pylint: disable=broad-except
+        #     self.response.write('There was an error running!')
+        # else:
+        #     self.response.write('http://ipa-hub.appspot.com/ipa?key='+k)
+        self.response.write('http://ipa-hub.appspot.com/ipa?key='+k)
 
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, resource):
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        self.send_blob(blob_info)
 
 class GetFile(webapp2.RequestHandler):
     def get(self):
@@ -97,6 +114,8 @@ class Plist(webapp2.RequestHandler):
         'bundle_id':e.bundle_id,
         'version':e.version,
         'file_url':'http://ipa-hub.appspot.com/get_file?key='+e.bundle_id+'_'+e.version}
+        if (e.blob_info_key) :
+            template_values['file_url'] = 'http://ipa-hub.appspot.com/serve/' + e.blob_info_key
         template = JINJA_ENVIRONMENT.get_template('ipa.plist')
         self.response.headers['Content-Type'] = "text/plain"
         self.response.write(template.render(template_values))
@@ -122,4 +141,5 @@ app = webapp2.WSGIApplication([
     ('/get_file', GetFile),
     ('/plist/(.*)', Plist),
     ('/ipa', IPA),
+    ('/serve/([^/]+)?', ServeHandler),
 ], debug=True)
